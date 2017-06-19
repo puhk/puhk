dd8221d8d6ecf72c2e73dee9ab634e29294e98f9
Jonny Taylor <jonny.taylor5@gmail.com>
2017-06-19 21:56:04 +0100

    dev game setup

--- public/init.js
+++ public/init.js
@@ -1,4 +1,4 @@
-const {GameCreator, Renderer} = window['nojball-game'];
+const {host, join, Renderer, Events} = window['nojball-game'];
 const renderer = new Renderer;
 
 const $mount = document.getElementById('mount');
@@ -10,11 +10,45 @@ const render = () => {
     renderer.setParent($mount)
         .setWidth($mount.offsetWidth)
         .setHeight($mount.offsetHeight)
-        .render();
+        .attach();
 };
 
-const host = (name, avatar) => {
-    game = GameCreator.host(renderer);
-    game.setLocalPlayer({ name, avatar });
-    render();
+const hostGame = () => {
+    host({
+        host: 'localhost',
+        path: 'p2p',
+        player: {
+            name: 'noj',
+            avatar: -1
+        },
+        renderer
+    }).then(res => {
+        game = res;
+        render();
+    });
+};
+
+const joinGame = () => {
+    join({
+        host: 'localhost',
+        path: 'p2p',
+        roomId: 'host',
+        player: {
+            name: 'noj',
+            avatar: -1
+        },
+        renderer
+    }).then(res => {
+        game = res;
+        render();
+    });
+};
+
+const moveToTeam = (player, team) => {
+    const event = new Events.ChangeTeam(game.getMe(), {
+        clientId: player,
+        team
+    });
+
+    game.addEvent(event);
 };
