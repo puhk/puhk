56e4188f21f7599174dfc744cd04e551de4ad6d2
Jonny Taylor <jonny.taylor5@gmail.com>
2020-01-01 18:01:42 +0000

    Add tests for multiple goals & no goals

--- src/engine/__tests__/goal-scored.spec.ts
+++ src/engine/__tests__/goal-scored.spec.ts
@@ -13,7 +13,7 @@ it.each([
     [new Vec(-2, 0), new Vec(5, 0), new Vec(0, -4), new Vec(0, 1)],
     [new Vec(-3, -6), new Vec(2, 5), new Vec(-1, 2), new Vec(3, -1)],
     [new Vec(7, -2), new Vec(-3, 5), new Vec(5, 1), new Vec(-2, 1)],
-])('returns goal scored if ball crosses goal line', (p0, p1, d1, d2) => {
+])('returns goal scored if ball crosses goal line %#', (p0, p1, d1, d2) => {
     const goal = createMock<Goal>({ p0, p1 });
 
     const oldState = createMock<State>({
@@ -28,4 +28,51 @@ it.each([
 
     const goalsScored = calculateGoalsScored(newState, oldState);
     expect(goalsScored).toHaveLength(1);
+});
+
+it('returns 2 goals scored if 2 balls cross goal line', () => {
+    const goal = createMock<Goal>({
+        p0: new Vec(0, 10),
+        p1: new Vec(0, -10)
+    });
+
+    const oldState = createMock<State>({
+        discs: [
+            createMock<Disc>({ id: 0, position: new Vec(-3, 7) }),
+            createMock<Disc>({ id: 1, position: new Vec(-2, -5) }),
+        ],
+        stadium: createMock<Stadium>({ goals: [goal] })
+    });
+
+    const newState = createMock<State>({
+        ...oldState,
+        discs: [
+            createMock<Disc>({ id: 0, position: new Vec(2, 3) }),
+            createMock<Disc>({ id: 1, position: new Vec(1, 1) }),
+        ],
+    });
+
+    const goalsScored = calculateGoalsScored(newState, oldState);
+    expect(goalsScored).toHaveLength(2);
+});
+
+it.each([
+    [new Vec(0, -10), new Vec(0, 10), new Vec(-5, -12), new Vec(5, -12)],
+    [new Vec(4, 0), new Vec(-4, 0), new Vec(-5, -5), new Vec(-5, 3)],
+    [new Vec(4, 0), new Vec(-4, 0), new Vec(0, 2), new Vec(0, 0)],
+])('returns no goal scored if ball hasnt crossed goal line %#', (p0, p1, d1, d2) => {
+    const goal = createMock<Goal>({ p0, p1 });
+
+    const oldState = createMock<State>({
+        discs: [createMock<Disc>({ position: d1 })],
+        stadium: createMock<Stadium>({ goals: [goal] })
+    });
+
+    const newState = createMock<State>({
+        ...oldState,
+        discs: [createMock<Disc>({ position: d2 })],
+    });
+
+    const goalsScored = calculateGoalsScored(newState, oldState);
+    expect(goalsScored).toHaveLength(0);
 });
