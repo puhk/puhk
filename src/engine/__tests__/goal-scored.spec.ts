8cd96178cfeffb4e9093d19de00841608071edb3
Jonny Taylor <jonny.taylor5@gmail.com>
2020-01-01 18:37:10 +0000

    Refactor common setup

--- src/engine/__tests__/goal-scored.spec.ts
+++ src/engine/__tests__/goal-scored.spec.ts
@@ -14,45 +14,31 @@ it.each([
     [new Vec(-3, -6), new Vec(2, 5), new Vec(-1, 2), new Vec(3, -1)],
     [new Vec(7, -2), new Vec(-3, 5), new Vec(5, 1), new Vec(-2, 1)],
 ])('returns goal scored if ball crosses goal line %#', (p0, p1, d1, d2) => {
-    const goal = createMock<Goal>({ p0, p1 });
-
-    const oldState = createMock<State>({
-        discs: [createMock<Disc>({ position: d1 })],
-        stadium: createMock<Stadium>({ goals: [goal] })
-    });
-
-    const newState = createMock<State>({
-        ...oldState,
-        discs: [createMock<Disc>({ position: d2 })],
-    });
-
-    const goalsScored = calculateGoalsScored(newState, oldState);
+    const goalsScored = calculate(
+        [createMock<Goal>({ p0, p1 })],
+        [createMock<Disc>({ position: d1 })],
+        [createMock<Disc>({ position: d2 })]
+    );
     expect(goalsScored).toHaveLength(1);
 });
 
 it('returns 2 goals scored if 2 balls cross goal line', () => {
-    const goal = createMock<Goal>({
-        p0: new Vec(0, 10),
-        p1: new Vec(0, -10)
-    });
-
-    const oldState = createMock<State>({
-        discs: [
+    const goalsScored = calculate(
+        [
+            createMock<Goal>({
+                p0: new Vec(0, 10),
+                p1: new Vec(0, -10)
+            })
+        ],
+        [
             createMock<Disc>({ id: 0, position: new Vec(-3, 7) }),
             createMock<Disc>({ id: 1, position: new Vec(-2, -5) }),
         ],
-        stadium: createMock<Stadium>({ goals: [goal] })
-    });
-
-    const newState = createMock<State>({
-        ...oldState,
-        discs: [
+        [
             createMock<Disc>({ id: 0, position: new Vec(2, 3) }),
             createMock<Disc>({ id: 1, position: new Vec(1, 1) }),
-        ],
-    });
-
-    const goalsScored = calculateGoalsScored(newState, oldState);
+        ]
+    );
     expect(goalsScored).toHaveLength(2);
 });
 
@@ -61,18 +47,28 @@ it.each([
     [new Vec(4, 0), new Vec(-4, 0), new Vec(-5, -5), new Vec(-5, 3)],
     [new Vec(4, 0), new Vec(-4, 0), new Vec(0, 2), new Vec(0, 0)],
 ])('returns no goal scored if ball hasnt crossed goal line %#', (p0, p1, d1, d2) => {
-    const goal = createMock<Goal>({ p0, p1 });
+    const goalsScored = calculate(
+        [createMock<Goal>({ p0, p1 })],
+        [createMock<Disc>({ position: d1 })],
+        [createMock<Disc>({ position: d2 })]
+    );
+    expect(goalsScored).toHaveLength(0);
+});
 
+const calculate = (
+    goals: Goal[],
+    oldDiscs: Disc[],
+    newDiscs: Disc[]
+) => {
     const oldState = createMock<State>({
-        discs: [createMock<Disc>({ position: d1 })],
-        stadium: createMock<Stadium>({ goals: [goal] })
+        discs: oldDiscs,
+        stadium: createMock<Stadium>({ goals })
     });
 
     const newState = createMock<State>({
         ...oldState,
-        discs: [createMock<Disc>({ position: d2 })],
+        discs: newDiscs
     });
 
-    const goalsScored = calculateGoalsScored(newState, oldState);
-    expect(goalsScored).toHaveLength(0);
-});
+    return calculateGoalsScored(newState, oldState);
+};
